fe02c826533f7b01404d09667b9280d56220b461
Nisarga Patel <np8@umbc.edu>
2015-11-02 12:14:49 -0500

    registration done

--- routes/index.js
+++ routes/index.js
@@ -8,62 +8,41 @@ function isRealValue(obj){
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', 
-                        taken: req.flash('taken'),
+  res.render('index', { title: 'Express',
                         ready: req.flash('ready'),
-                        match: req.flash('match')
+                        error: req.flash('error')
                       });
 });
 
 
 router.post('/register', function(req,res,next) {
     
-    var User = Parse.Object.extend("User");
-    var user = new User();
-    var creds = {
-        username : req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        password_again: req.body.password_again
-    }
-    var numErrors  = 0;
-    var query = new Parse.Query(User);
-    query.equalTo("username", req.body.username);
-    query.find({
-        success:function(list) {
-            // the object has been found!
-
-            if(list.length != 0){
+    var user = new Parse.User();
+    user.set("username", req.body.username);
+    user.set("password", req.body.password);
+    user.set("email", req.body.email);
+    if(req.body.password == req.body.password_again){
+    
+        user.signUp(null, {
+            success: function(user) {
                 
-                req.flash('taken', 'Username already taken!');
+                req.flash('ready', "You are now ready to login!");
                 res.redirect('/');
-//                res.redirect("/");
-                ++numErrors;
+                
+            },
+            error: function(user, error) {
+                
+                //req.flash('error', "Error: " + error.code + " " + error.message);
+                req.flash('error', error.message);
+                res.redirect('/');
             }
-        },
-        error: function(error) {
-            // error is an instance of Parse.Error.
-            console.log(error.message);
-        }
-    });
-    
-    // check if passwords match
-    if(creds.password == creds.password_again && numErrors == 0){
-    
-        user.save(creds).then(function(object) {
-            console.log("user " + creds.username + " added!");
         });
-
-        req.flash('ready', "You are now ready to login!");
-        res.redirect('/');
         
     }else {
         
-        req.flash('match', "Passwords do not match!");
-        ++numErrors;
-    }
-    if(numErrors > 0){
+        req.flash('error', "passwords must match");
         res.redirect('/');
+        
     }
 });
 
